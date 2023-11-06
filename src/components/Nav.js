3db3b6050e190fe193c45eed84faeade3824cdd5
Mesut Biskin <msbiskin@gmail.com>
2023-11-06 16:44:43 -0500

    ADDED README.md

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -8,10 +8,10 @@ export default function Nav() {
   return (
     <header>
       <ul>
-        <li><a href="/" target="_blank" rel="noopener noreferrer">Home</a></li>
-        <li><a href="/new-post" target="_blank" rel="noopener noreferrer">Add Post</a></li>
-        <li><a href="/login" target="_blank" rel="noopener noreferrer">Login</a></li>
-        <li><a href="/signup" target="_blank" rel="noopener noreferrer">Sign up</a></li>
+        <li><a href="/"  rel="noopener noreferrer">Home</a></li>
+        <li><a href="/new-post"  rel="noopener noreferrer">Add Post</a></li>
+        <li><a href="/login"  rel="noopener noreferrer">Login</a></li>
+        <li><a href="/signup" rel="noopener noreferrer">Sign up</a></li>
         <li style={{color: 'white'}}>{userState.email}</li>
       </ul>
     </header>
